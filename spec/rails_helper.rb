156d94b97973f92ffce301606c3365d5e8ac2e60
André Heijstek <andre.heijstek@hey.com>
2025-12-28 20:11:46 +0100

    first failing (pending) test

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -69,11 +69,4 @@ RSpec.configure do |config|
   config.filter_rails_from_backtrace!
   # arbitrary gems may also be filtered via:
   # config.filter_gems_from_backtrace("gem name")
-
-  config.include FactoryBot::Syntax::Methods
-  config.filter_rails_from_backtrace
-
-  config.before(:each, type: :system) do
-      driven_by :rack_test
-    end
 end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -46,6 +46,7 @@ RSpec.configure do |config|
 
 # The settings below are suggested to provide a good initial experience
 # with RSpec, but feel free to customize to your heart's content.
+=begin
   # This allows you to limit a spec run to individual examples or groups
   # you care about by tagging them with `:focus` metadata. When nothing
   # is tagged with `:focus`, all examples get run. RSpec also provides
@@ -89,11 +90,5 @@ RSpec.configure do |config|
   # test failures related to randomization by passing the same `--seed` value
   # as the one that triggered the failure.
   Kernel.srand config.seed
-end
-
-Shoulda::Matchers.configure do |config|
-  config.integrate do |with|
-    with.test_framework :rspec
-    with.library :rails
-  end
+=end
 end

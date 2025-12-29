3f6affa42727f219d08fdd18b30dc3404657b9f9
André Heijstek <andre.heijstek@hey.com>
2025-12-29 23:34:33 +0100

    Some useful refactorings

--- spec/support/pages/HomesNewPage.rb
+++ spec/support/pages/HomesNewPage.rb
@@ -1,7 +1,12 @@
-# Page object for the /homeconfiguration page
+# Page object for the Homes "new" page
 class HomesNewPage < SitePrism::Page
   set_url "/homes/new"
 
-  element :description_field, "#home_description"
-  element :create_home_button, 'input[type="submit"][value="Create Home"]'
+  element :description, "#home_description"
+  element :submit, 'input[type="submit"][value="Create Home"]'
+
+  def create_home(description:)
+    self.description.set(description)
+    submit.click
+  end
 end
--- spec/system/setup_home_configurations_spec.rb
+++ spec/system/setup_home_configurations_spec.rb
@@ -2,18 +2,10 @@ require 'rails_helper'
 
 RSpec.describe "SetupHomeConfigurations", type: :system do
   it 'creates a new home' do
-    visit "/homes"
-
-    hop = HomesOverviewPage.new
-    hop.load
-    save_page("tmp/page.html")
-    hop.new_home_link.click
-
     hnp = HomesNewPage.new
-    hnp.description_field.set "Thuis"
-    hnp.create_home_button.click
+    hnp.load
+    hnp.create_home(description: "Thuis")
 
-    hop.load
     expect(page).to have_content ("Thuis")
   end
 end
